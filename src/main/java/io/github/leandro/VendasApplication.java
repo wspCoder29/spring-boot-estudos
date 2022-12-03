ada131f7c103436ae82d41b2ac222d1efbb30aa7
unknown <leandro.wsp@gmail.com>
2022-12-03 18:43:15 -0300

    hello world

--- src/main/java/io/github/leandro/VendasApplication.java
+++ src/main/java/io/github/leandro/VendasApplication.java
@@ -1,15 +1,23 @@
 package io.github.leandro;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "HelloWorld!";
+    }
+
     public static void main(String[] args) {
+        //run para rodar a applicação rececebe a classe e os args
         SpringApplication.run(VendasApplication.class, args);
-        //test
+
 
 
     }
