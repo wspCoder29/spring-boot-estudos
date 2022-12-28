300f530fb3956fa2fade5db5c6e38cd39874dd70
Leandro <leandro.wsp@gmail.com>
2022-12-27 23:00:49 -0300

    h2+classes-ok

---
+++ src/main/java/io/github/leandro/io/domain/entity/Cliente.java
@@ -0,0 +1,31 @@
+package io.github.leandro.io.domain.entity;
+
+public class Cliente {
+
+
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+
+
+
+}
