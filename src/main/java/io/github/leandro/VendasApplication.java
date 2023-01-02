0b57b8a7f9bf3854531cf14d5c5e01cce8fd8f59
Leandro <leandro.wsp@gmail.com>
2023-01-01 23:47:27 -0300

    Salvando cliente_ok

--- src/main/java/io/github/leandro/VendasApplication.java
+++ src/main/java/io/github/leandro/VendasApplication.java
@@ -25,6 +25,5 @@ public class VendasApplication {
         SpringApplication.run(VendasApplication.class, args);
 
 
-
     }
 }
--- src/main/java/io/github/leandro/domain/repositorio/Clientes.java
+++ src/main/java/io/github/leandro/domain/repositorio/Clientes.java
@@ -2,6 +2,8 @@ package io.github.leandro.domain.repositorio;
 
 import io.github.leandro.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,15 +12,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class Clientes {
 
+
+    private static String INSERT = "insert into cliente (nome) values (?) ";
+
     @Autowired
    private JdbcTemplate jdbcTemplate;
 
+    @Bean
+    public CommandLineRunner init (@Autowired Clientes clientes){
+        //command line runner
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Leandro");
+            clientes.salvar(cliente);
+        };
+    }
 
-
+    
     public Cliente salvar(Cliente cliente){
-
-        return null;
-
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        return cliente;
     }
 
 
